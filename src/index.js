5c8aba42cddb90883be9949b8a32c17a1d808435
Charwekey <charwekeyrabisabutey@gmail.com>
2025-04-24 23:11:50 +1200

    Added All files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+// function handleSearch(event) {
+//   event.preventDefault();
+//   let searchInput = document.querySelector("#search-input");
+//   let cityElement = document.querySelector("#city");
+//   cityElement.innerHTML = searchInput.value;
+// }
 
+// let searchForm = document.querySelector("#search-form");
+// searchForm.addEventListener("submit", handleSearch); 
+
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Accra");
